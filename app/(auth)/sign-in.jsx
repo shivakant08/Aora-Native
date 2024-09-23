3d574c5e2aeec34ebc9a148bd2c5ad4d6efc5cda
shivakant08 <shivakantgiri07@gmail.com>
2024-09-23 18:14:54 +0530

    Auth Functionality

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full h-full justify-center px-4 my-6">
+          <Image
+            source={images.logo}
+            className="w-[115px] h-[35px]"
+            resizeMode="contain"
+          />
+          <Text className="text-2xl text-white mt-10 font-psemibold text-semibold">
+            Log in to Aora
+          </Text>
+          <FormField />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,18 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { StatusBar } from "react-native";
-import { Link } from "expo-router";
+
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ScrollView } from "react-native";
 import { images } from "../constants";
 import CustomButton from "../components/CustomButton";
+import { StatusBar } from "expo-status-bar";
 
 const Home = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="justify-center items-center w-full h-full px-4">
+        <View className="justify-center items-center w-full min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -40,11 +41,12 @@ const Home = () => {
           </Text>
           <CustomButton
             title="Continue with email"
-            handlePress={() => {}}
+            handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
           />
         </View>
       </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 };
