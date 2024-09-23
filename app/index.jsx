3b76398081eff76adeff38ef7a9b0946f465c3ac
shivakant08 <shivakantgiri07@gmail.com>
2024-09-23 14:10:27 +0530

    creation of home page

---
+++ app/index.jsx
@@ -0,0 +1,52 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StatusBar } from "react-native";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView } from "react-native";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
+
+const Home = () => {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="justify-center items-center w-full h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            resizeMode="contain"
+            className="max-w-[380px] w-full h-[300px]"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-center font-bold text-3xl text-white">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on the journey of
+            limitless exploration with Aora
+          </Text>
+          <CustomButton
+            title="Continue with email"
+            handlePress={() => {}}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
